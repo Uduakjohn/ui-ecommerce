4b66594f22864721c0222c2ca1dfab1da8076ffd
Uduakjohn <Johnuduak035@gmail.com>
2022-10-21 21:20:56 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1607619056574-7b8d3ee536b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZHJ1Z3N8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+      title: "GOOD HEALTH",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1579165466991-467135ad3110?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFib3JhdG9yeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      title: "LABORATORY SERVICES",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR Tests.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1582560475093-ba66accbc424?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGFib3JhdG9yeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+      title: "HEALTH FIRST",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR ALL TEST.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://media.istockphoto.com/photos/spoonful-of-golden-syrup-overflowing-from-it-picture-id182661145?b=1&k=20&m=182661145&s=170667a&w=0&h=Ky03KH087kdX4zyAQhx4EiRudgDkk4SbwCEJ1dfKpOM=",
+      title: "SYRUPS!",
+    },
+    {
+      id: 2,
+      img: "https://media.istockphoto.com/photos/bunch-of-pills-of-different-colors-in-blisters-pack-closeup-pharmacy-picture-id1291589526?b=1&k=20&m=1291589526&s=170667a&w=0&h=ZOiyW4-CVQjDTjMeXB5ELid4clRvKJdAu7Zo3NEzKbA=",
+      title: "TABLETS",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1584912262782-d828d56a1d9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fwc3VsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
+      title: "Capsules",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://media.istockphoto.com/photos/asthma-relief-spray-isolated-on-white-background-with-clipping-paths-picture-id1221542235?b=1&k=20&m=1221542235&s=170667a&w=0&h=8dPsy90EQhrUgKdLGZD6GyTCRliCfoolh8dv2J5R3Rc=",
+    },
+    {
+      id:2,
+      img:"https://media.istockphoto.com/photos/generic-paracetamol-isolated-on-white-picture-id157402355?k=20&m=157402355&s=612x612&w=0&h=HJbfdh-GpHW4_x5caeWPesLQZIkBA_4w26P2B5I5AvM=",
+    },
+    {
+      id:3,
+      img:"https://media.istockphoto.com/photos/cropped-hand-of-doctor-injecting-patient-picture-id1347166052?b=1&k=20&m=1347166052&s=170667a&w=0&h=CbhrNDWKbGsbzMZ_1xntCzWbOh0LSZM9bDtgQhgWwBU=",
+    },
+    {
+      id:4,
+      img:"https://images.unsplash.com/photo-1593181581874-361761582b9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2VsbHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    },
+    {
+      id:5,
+      img:"https://media.istockphoto.com/photos/ibuprofen-is-a-drug-used-to-treat-pain-fever-and-inflammation-on-picture-id1390115588?b=1&k=20&m=1390115588&s=170667a&w=0&h=farAsSepYtvOPIRFoUJsAbza5Iv3lsFQ9hsye11a7Qw=",
+    },
+    {
+      id:6,
+      img:"https://media.istockphoto.com/photos/corticosteroid-medication-or-drug-concept-photo-on-doctor-table-lies-picture-id1172213343?b=1&k=20&m=1172213343&s=170667a&w=0&h=h2Bly2pBZbzc-LX9mWBKGoGL68D2kLXKscQwLpRqV2Y=",
+    },
+    {
+      id:7,
+      img:"https://media.istockphoto.com/photos/dangerous-opioids-picture-id1061027986?b=1&k=20&m=1061027986&s=170667a&w=0&h=OA9-CJiJ1JokoHgLr-tPJ5Q5Z7ai1DA_pPci-8TKFOs=",
+    },
+    {
+      id:8,
+      img:"https://images.unsplash.com/photo-1512069511692-b82d787265cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGFjY3UlMjBjaGVja3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+    },
+  ]
